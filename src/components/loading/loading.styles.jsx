f5421e6127a856eb93135393fcb34e70d2f97b2f
De Sean Ward <dward1711@gmail.com>
2023-07-31 18:38:34 -0500

    Centering of the Loading status

--- src/components/loading/loading.styles.jsx
+++ src/components/loading/loading.styles.jsx
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
 export const LoadingContainer = styled.div`
+	position: absolute;
 	width: 100%;
 	height: 100%;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+	bottom: 6em;
 `;
 
-export const LoadingImageContainer = styled.div`
-`;
+export const LoadingImageContainer = styled.div``;
